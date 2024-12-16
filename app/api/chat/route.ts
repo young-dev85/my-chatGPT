4ca4033cf1775545a5bc751015b4dd658d747cc5
young-dev85 <doubley0007@gmail.com>
2024-12-16 20:00:26 +0900

    update maxDuration to 60seconds

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -2,7 +2,7 @@ import { openai } from "@ai-sdk/openai";
 import { streamText } from "ai";
 
 // Allow streaming responses up to 30 seconds
-export const maxDuration = 30;
+export const maxDuration = 60;
 
 export async function POST(req: Request) {
   const {
